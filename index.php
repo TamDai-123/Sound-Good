ae64f42b8cc140580f74686def44a4e310d92cc8
Tamdai <nontasit2411@gmail.com>
2025-12-14 13:51:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>SoundGood</title>
+<link rel="icon" type="image/png" href="img/2.png">
 <style>
 * { margin:0; padding:0; box-sizing:border-box; }
 body { font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background:#fff; padding:20px;}
